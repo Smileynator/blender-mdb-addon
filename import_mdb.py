83fdbff1bab8d3c24a31b55cdb88715530ade54f
BlueAmulet <43395286+BlueAmulet@users.noreply.github.com>
2021-08-25 00:25:57 -0600

    Fix normal maps
    Flip Y for OpenGL
    Handle unswizzle at texture load time instead of adding to shader
        I'm unsure if this is the right thing to do

--- import_mdb.py
+++ import_mdb.py
@@ -357,89 +357,6 @@ def load(operator, context, filepath='', **kwargs):
     # Texture cache
     textures = {}
 
-    # Create unswizzle node group
-    if bpy.data.node_groups.get('Normal Unswizzle') is None:
-        nspace = 160
-        unswizzle = bpy.data.node_groups.new('Normal Unswizzle', 'ShaderNodeTree')
-
-        group_inputs = unswizzle.nodes.new('NodeGroupInput')
-        group_inputs.location[0] = nspace * 0
-        unswizzle.inputs.new('NodeSocketColor', 'Color')
-        unswizzle.inputs.new('NodeSocketFloat', 'Alpha')
-
-        splitRGB = unswizzle.nodes.new('ShaderNodeSeparateRGB')
-        splitRGB.location[0] = nspace * 1
-        unswizzle.links.new(splitRGB.inputs['Image'], group_inputs.outputs['Color'])
-
-        valR = unswizzle.nodes.new('ShaderNodeMath')
-        valR.location[0] = nspace * 2
-        valR.operation = 'MULTIPLY'
-        unswizzle.links.new(valR.inputs[0], splitRGB.outputs['R'])
-        unswizzle.links.new(valR.inputs[1], group_inputs.outputs['Alpha'])
-
-        mulR = unswizzle.nodes.new('ShaderNodeMath')
-        mulR.location[0] = nspace * 3
-        mulR.operation = 'MULTIPLY_ADD'
-        unswizzle.links.new(mulR.inputs[0], valR.outputs['Value'])
-        mulR.inputs[1].default_value = 2.0
-        mulR.inputs[2].default_value = -1.0
-
-        mulG = unswizzle.nodes.new('ShaderNodeMath')
-        mulG.location[0] = nspace * 3
-        mulG.location[1] = mulG.location[1] - 170
-        mulG.operation = 'MULTIPLY_ADD'
-        unswizzle.links.new(mulG.inputs[0], splitRGB.outputs['G'])
-        mulG.inputs[1].default_value = 2.0
-        mulG.inputs[2].default_value = -1.0
-
-        powR = unswizzle.nodes.new('ShaderNodeMath')
-        powR.location[0] = nspace * 4
-        powR.operation = 'MULTIPLY'
-        unswizzle.links.new(powR.inputs[0], mulR.outputs['Value'])
-        unswizzle.links.new(powR.inputs[1], mulR.outputs['Value'])
-
-        powG = unswizzle.nodes.new('ShaderNodeMath')
-        powG.location[0] = nspace * 4
-        powG.location[1] = powG.location[1] - 170
-        powG.operation = 'MULTIPLY'
-        unswizzle.links.new(powG.inputs[0], mulG.outputs['Value'])
-        unswizzle.links.new(powG.inputs[1], mulG.outputs['Value'])
-
-        subR = unswizzle.nodes.new('ShaderNodeMath')
-        subR.location[0] = nspace * 5
-        subR.operation = 'SUBTRACT'
-        subR.inputs[0].default_value = 1.0
-        unswizzle.links.new(subR.inputs[1], powR.outputs['Value'])
-
-        subG = unswizzle.nodes.new('ShaderNodeMath')
-        subG.location[0] = nspace * 6
-        subG.operation = 'SUBTRACT'
-        unswizzle.links.new(subG.inputs[0], subR.outputs['Value'])
-        unswizzle.links.new(subG.inputs[1], powG.outputs['Value'])
-
-        sqrtB = unswizzle.nodes.new('ShaderNodeMath')
-        sqrtB.location[0] = nspace * 7
-        sqrtB.operation = 'SQRT'
-        unswizzle.links.new(sqrtB.inputs[0], subG.outputs['Value'])
-
-        valB = unswizzle.nodes.new('ShaderNodeMath')
-        valB.location[0] = nspace * 8
-        valB.operation = 'MULTIPLY_ADD'
-        unswizzle.links.new(valB.inputs[0], sqrtB.outputs['Value'])
-        valB.inputs[1].default_value = 0.5
-        valB.inputs[2].default_value = 0.5
-
-        combineRGB = unswizzle.nodes.new('ShaderNodeCombineRGB')
-        combineRGB.location[0] = nspace * 9
-        unswizzle.links.new(combineRGB.inputs['R'], valR.outputs['Value'])
-        unswizzle.links.new(combineRGB.inputs['G'], splitRGB.outputs['G'])
-        unswizzle.links.new(combineRGB.inputs['B'], valB.outputs['Value'])
-
-        group_outputs = unswizzle.nodes.new('NodeGroupOutput')
-        group_outputs.location[0] = nspace * 10
-        unswizzle.outputs.new('NodeSocketColor', 'Color')
-        unswizzle.links.new(group_outputs.inputs['Color'], combineRGB.outputs['Image'])
-
     # Create materials
     materials = []
     for mdb_material in mdb['materials']:
@@ -456,8 +373,24 @@ def load(operator, context, filepath='', **kwargs):
                 texImage.image = textures[filename]
             else:
                 try:
-                    texImage.image = bpy.data.images.load(os.path.join(os.path.dirname(filepath), '..', 'HD-TEXTURE', filename))
-                    textures[filename] = texImage.image
+                    image = bpy.data.images.load(os.path.join(os.path.dirname(filepath), '..', 'HD-TEXTURE', filename))
+                    texImage.image = image
+                    textures[filename] = image
+                    if texture['map'] == 'normal':
+                        image.colorspace_settings.name = 'Non-Color'
+                        np_pxl = np.empty(len(image.pixels), dtype=np.float32)
+                        image.pixels.foreach_get(np_pxl)
+                        np_pxl.shape = (len(image.pixels) // image.channels, image.channels)
+                        np_pxl[:, 0] *= np_pxl[:, 3] # Reconstruct X
+                        np_pxl[:, 1] = 1 - np_pxl[:, 1] # Flip Y (DX -> GL)
+                        np_pxl[:, 2] = (np.sqrt(1 - np.clip(np.square(np_pxl[:, 0] * 2. - 1.) + np.square(np_pxl[:, 1] * 2. - 1.), 0., 1.)) + 1.) * .5 # Reconstruct Z
+                        np_pxl[:, 3] = 1 # Blank Alpha
+                        np_pxl.shape = (len(image.pixels))
+
+                        # I do not understand why, but setting, packing, and setting again, makes the changes actually apply.
+                        image.pixels.foreach_set(np_pxl)
+                        image.pack()
+                        image.pixels.foreach_set(np_pxl)
                 except RuntimeError as e: # Ignore texture import error
                     print(e)
             if texture['map'] == 'albedo':
@@ -467,24 +400,15 @@ def load(operator, context, filepath='', **kwargs):
                 material.node_tree.links.new(bsdf.inputs['Alpha'], texImage.outputs['Alpha'])
             elif texture['map'] == 'normal':
                 if texImage.image is not None:
-                    texImage.image.colorspace_settings.name = 'Linear'
-                texImage.location[0] = bsdf.location[0] - 700
+                    texImage.image.colorspace_settings.name = 'Non-Color'
+                texImage.location[0] = bsdf.location[0] - 500
                 texImage.location[1] = bsdf.location[1] - 510
 
-                # Unswizzle normal map
-                unswizzle = material.node_tree.nodes.new('ShaderNodeGroup')
-                unswizzle.location[0] = bsdf.location[0] - 400
-                unswizzle.location[1] = bsdf.location[1] - 510
-                unswizzle.node_tree = bpy.data.node_groups.get('Normal Unswizzle')
-                unswizzle.show_options = False
-                material.node_tree.links.new(unswizzle.inputs['Color'], texImage.outputs['Color'])
-                material.node_tree.links.new(unswizzle.inputs['Alpha'], texImage.outputs['Alpha'])
-
-                # Connect fixed normal map
+                # Connect normal map
                 normalMap = material.node_tree.nodes.new('ShaderNodeNormalMap')
                 normalMap.location[0] = bsdf.location[0] - 200
                 normalMap.location[1] = bsdf.location[1] - 510
-                material.node_tree.links.new(normalMap.inputs['Color'], unswizzle.outputs['Color'])
+                material.node_tree.links.new(normalMap.inputs['Color'], texImage.outputs['Color'])
                 material.node_tree.links.new(bsdf.inputs['Normal'], normalMap.outputs['Normal'])
             else:
                 texImage.location[0] = bsdf.location[0] - 700 + unhandled * 40
